19f450827a2238f63abd224cb5cef85ec0b18309
Apozharsky <43452450+Towermoni@users.noreply.github.com>
2019-09-28 21:21:17 -0500

    Weapons
    These are the weapons available in the game. Each weapon class implements a "Weapons" interface. More weapons will be added in the future.

---
+++ Weapons/Melee/Dagger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dagger : MonoBehaviour, IWeapon
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public double CriticalRating { get; set; }
+    public double CriticalChance { get; set; }
+    public double Deflect { get; set; }
+    public int Value { get; set; }
+    public double Weight { get; set; }
+    public double rnd { get; set; }
+
+    public Dagger()
+    {
+        ID = 002;
+        Name = "Dagger";
+        Damage = 2;
+        CriticalRating = 4;
+        CriticalChance = 2;
+        Deflect = .5;
+        Value = 10;
+        Weight = 5;
+    }
+
+
+    public double checkDamage(Character character)
+    {
+        rnd = Random.Range(1, 10);
+        double damage = 0;
+
+        if (rnd <= CriticalChance)
+        {
+            damage = (character.Modifiers[0] + Damage) * CriticalRating;
+        }
+        else
+        {
+            damage = character.Modifiers[0] + Damage;
+        }
+
+        return damage;
+    }
+}
---
+++ Weapons/Melee/LongSword.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongSword : MonoBehaviour, IWeapon
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public double CriticalRating { get; set; }
+    public double CriticalChance { get; set; }
+    public double Deflect { get; set; }
+    public int Value { get; set; }
+    public double Weight { get; set; }
+    public double rnd { get; set; }
+
+    public LongSword()
+    {
+        ID = 000;
+        Name = "Long Sword";
+        Damage = 3;
+        CriticalRating = 3;
+        CriticalChance = 1.5;
+        Deflect = 1.5;
+        Value = 25;
+        Weight = 10;
+    }
+
+
+    public double checkDamage(Character character)
+    {
+        rnd = Random.Range(1, 10);
+        double damage = 0;
+
+        if (rnd <= CriticalChance)
+        {
+            damage = (character.Modifiers[0] + Damage) * CriticalRating;
+        }
+        else
+        {
+            damage = character.Modifiers[0] + Damage;
+        }
+
+        return damage;
+    }
+}
